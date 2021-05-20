9d63e324fb940132c2c47b938ccf9449f49c6558
Monica Lara <ptts.no2020@gmail.com>
2021-05-20 22:32:54 +0200

    Commit#Mlo-Calculo saldo actual fechas

--- src/main/java/com/ingenia/banca/repository/CuentaRepository.java
+++ src/main/java/com/ingenia/banca/repository/CuentaRepository.java
@@ -4,8 +4,6 @@ import com.ingenia.banca.model.Cuenta;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import java.time.LocalDate;
-import java.util.List;
 
 public interface CuentaRepository extends JpaRepository<Cuenta, Long> {
 
